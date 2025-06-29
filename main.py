080a3cfb81826a2fd7744537ee818949f80a531c
Harshit Raj <115299833+rajharshit21@users.noreply.github.com>
2025-06-30 03:24:05 +0530

    Updateds main.py

--- main.py
+++ main.py
@@ -45,7 +45,10 @@ TEMP_DIR.mkdir(exist_ok=True)
 # === CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://subsonic.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
